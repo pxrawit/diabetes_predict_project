79b2a23aa9e744eb64c93dbb0be6a7efe3e445b3
pxrawit <puwaritchammunkung@gmail.com>
2026-08-19 22:23:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -454,7 +454,7 @@ with tabs[3]:
 with tabs[4]:
     st.markdown('<div class="main-header">👨‍💻 เกี่ยวกับผู้พัฒนา</div>', unsafe_allow_html=True)
     
-  profile_image = "https://github.com/pxrawit/diabetes_predict_project/raw/main/profile.jpg"
+  profile_image = "https://github.com/pxrawit/diabetes_predict_project/blob/main/profile.jpg"
     
     st.markdown(f'''
         <div class="developer-card">
